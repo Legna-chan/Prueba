5768835a12181fb9ceb5a9c0e9e1b2efc2511a67
Legna ❀ <Beatrizmanjarreztorres6@gmail.com>
2025-06-25 09:41:32 -0700

    Crear api/ip.js

---
+++ api/ip.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  console.log(`IP capturada: ${ip}`);
+
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send(`
+    <h1>Tu celular está siendo hackeado... 🤖</h1>
+    <p>Es broma 😅</p>
+  `);
+}
